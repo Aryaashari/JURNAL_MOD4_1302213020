866142cd022cb63f6e16670395fc2c8f8a425264
PC19-IFLAB <PC19-IFLAB>
2023-03-13 07:20:50 +0700

    menambahkan class KodeBuah

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class KodeBuah
+{
+    static Dictionary<string, string> Kamus = new Dictionary<string, string>() {
+        {"Apel","A00"},
+        {"Aprikot","B00"},
+        {"Alpukat","C00"},
+        {"Pisang","D00"},
+        {"Paprika","E00"},
+        {"Blackberry","F00"},
+        {"Ceri","H00"},
+        {"Kelapa","I00"},
+        {"Jagung","J00"},
+        {"Kurma","K00"},
+        {"Durian","L00"},
+        {"Anggur","M00"},
+        {"Melon","N00"},
+        {"Semangka","O00"},
+    };
+
+    public string getKodeBuah(string buah)
+    {
+        if (Kamus.ContainsKey(buah))
+        {
+            return Kamus[buah];
+        }
+
+        return "Kode buah not found!";
+    }
+
+}
